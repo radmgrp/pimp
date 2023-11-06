45f3b1a476550bab8d67a51ad1bd99bdb7838a1f
Mikhail Shayunou <radmgrp@gmail.com>
2023-11-06 14:16:41 +0100

    new version

--- passimpayapinew2/PassimPayApiNew2.py
+++ passimpayapinew2/PassimPayApiNew2.py
@@ -2,10 +2,11 @@ import hmac
 import requests
 import json
 import hashlib
+from collections import OrderedDict
 
 
 class PassimpayApi:
-    URL_BASE = 'https://passimpay.io/api'
+    URL_BASE = ('https://api.passimpay.io')
 
     def __init__(self, platform_id, secret_key):
         self.platform_id = platform_id
@@ -19,20 +20,16 @@ class PassimpayApi:
             raise Exception('Passimpay: platform id can not be empty.')
 
         url = f'{self.URL_BASE}/{endpoint}'
-        payload = {'platform_id': self.platform_id}
+        # payload = {'platform_id': self.platform_id}
+        #
+        # if data:
+        #     payload.update(data)
 
-        if data:
-            payload.update(data)
+        payload_str = "&".join(f"{key}={value}" for key, value in data.items()).encode('utf-8')
 
-        payload_str = "&".join(f"{key}={value}" for key, value in payload.items()).encode('utf-8')
         hash_value = hmac.new(self.secret_key.encode('utf-8'), payload_str, hashlib.sha256).hexdigest()
+        data['hash'] = hash_value
 
-        data = {
-            'platform_id': self.platform_id,
-            'hash': hash_value,
-        }
-
-        headers = {'Content-Type': 'application/json'}
         response = requests.post(url, data=data)
 
         return response.json()
@@ -60,71 +57,71 @@ class PassimpayApi:
         return currencies, error
 
     def invoice(self, id, amount):
-        data = {
-            'order_id': id,
-            'amount': amount
-        }
+        data = OrderedDict([
+            ('order_id', id),
+            ('amount', amount)
+        ])
         response = self._make_request('createorder', data)
         url, error = response.get('url'), response.get('message')
         self.check_and_print(f'Invoice URL: {url}', error)
         return url, error
 
     def invoice_status(self, id):
-        data = {'order_id': id}
+        data = OrderedDict([
+            ('order_id', id)
+        ])
         response = self._make_request('orderstatus', data)
         status, error = response.get('status'), response.get('message')
         self.check_and_print(f'Invoice Status: {status}', error)
         return status, error
 
     def payment_wallet(self, order_id, payment_id):
-        data = {
-            'payment_id': payment_id,
-            'platform_id': self.platform_id,
-            'order_id': order_id
-        }
+        data = OrderedDict([
+            ('payment_id', payment_id),
+            ('platform_id', self.platform_id),
+            ('order_id', order_id)
+        ])
         response = self._make_request('getpaymentwallet', data)
         address, error = response.get('address'), response.get('message')
         self.check_and_print(f'Payment Wallet Address: {address}', error)
         return address, error
 
     def withdraw(self, payment_id, address_to, amount):
-        data = {
-            'payment_id': payment_id,
-            'platform_id': self.platform_id,
-            'amount': amount,
-            'address_to': address_to
-        }
+        data = OrderedDict([
+            ('payment_id', payment_id),
+            ('platform_id', self.platform_id),
+            ('amount', amount),
+            ('address_to', address_to)
+        ])
         response = self._make_request('withdraw', data)
         self.check_and_print(f'Withdrawal Response: {response}', response.get('message'))
         return response, response.get('message')
 
     def transaction_status(self, tx_hash):
-        data = {'txhash': tx_hash}
+        data = OrderedDict([
+            ('txhash', tx_hash)
+        ])
         response = self._make_request('transactionstatus', data)
         self.check_and_print(f'Transaction Status: {response}', response.get('message'))
         return response, response.get('message')
 
 
 # Пример использования
-platform_id = 'your_platform_id'
-secret_key = 'your_secret_key'
-
-passimpay_api = PassimpayApi(platform_id, secret_key)
-
-passimpay_api.balance()
-passimpay_api.currencies()
-
-order_id = 'your_order_id'
-passimpay_api.invoice_status(order_id)
-
-order_id = 'your_order_id'
-payment_id = 'your_payment_id'
-passimpay_api.payment_wallet(order_id, payment_id)
-
-payment_id = 'your_payment_id'
-address_to = 'recipient_address'
-amount = 100.0
-passimpay_api.withdraw(payment_id, address_to, amount)
-
-tx_hash = 'your_transaction_hash'
-passimpay_api.transaction_status(tx_hash)
+# platform_id = 'your platform_id'
+# secret_key = 'your secret_key'
+#
+# passimpay_api = PassimpayApi(platform_id, secret_key)
+#
+# passimpay_api.balance()
+# passimpay_api.currencies()
+#
+# order_id = 'your_order_id'
+# passimpay_api.invoice_status(order_id)
+#
+# order_id = 'your_order_id'
+# payment_id = 'your_payment_id'
+# passimpay_api.payment_wallet(order_id, payment_id)
+#
+#
+# tx_hash = 'your_transaction_hash'
+# passimpay_api.transaction_status(tx_hash)
